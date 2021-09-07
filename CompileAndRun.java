33506c32ec139c63a7dc0545f2edac09fe8a5f0c
Kyle Davelaar <kyledavelaar@gmail.com>
2021-09-07 16:58:24 -0700

    initial

---
+++ CompileAndRun.java
@@ -0,0 +1,9 @@
+/*
+  1. compile: javac CompileAndRun.java
+  2. run: java CompileAndRun x y
+*/
+public class CompileAndRun {
+  public static void main(String[] args) {
+    System.out.println(args[0] + " " + args[1]);
+  }
+}
